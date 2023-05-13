4867ee8bd8963e42c31507e255fdad9209413dfb
achu94 <aygyun_94@outlook.de>
2023-05-13 22:41:20 +0200

    qwr

--- src/src/GameScene.ts
+++ src/src/GameScene.ts
@@ -40,7 +40,7 @@ export class GameScene extends Phaser.Scene {
     this.initScoreText();
 
     const sound = this.sound.add('mainSound');
-    sound.play({loop: true});
+    sound.play({loop: true, volume: 0.1});
 
     this.add.text(20, 580, 'Lider: GÜni Score: 1050', { font: '16px Arial' });
 
