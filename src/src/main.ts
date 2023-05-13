7c8a370e18c825b7ac66fe09ffb3469fe103f944
achu94 <aygyun_94@outlook.de>
2023-05-13 22:23:53 +0200

    some css.

--- src/src/main.ts
+++ src/src/main.ts
@@ -3,18 +3,18 @@ import Phaser from 'phaser';
 import './style.css';
 
 const config: Phaser.Types.Core.GameConfig = {
-    width: 800,
-    height: 600,
-    type: Phaser.AUTO,
-    parent: 'game-container',
-    physics: {
-      default: 'arcade',
-      arcade: {
-        gravity: { y: 0 },
-        debug: false
-      }
-    },
-    scene: [GameScene]
-  };
+  width: 800,
+  height: 600,
+  type: Phaser.AUTO,
+  parent: 'game-container',
+  physics: {
+    default: 'arcade',
+    arcade: {
+      gravity: { y: 0 },
+      debug: false
+    }
+  },
+  scene: [GameScene]
+};
 
 new Phaser.Game(config);
